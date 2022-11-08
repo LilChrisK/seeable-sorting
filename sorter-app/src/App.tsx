23f1138101a70928b144411e15693251b83e5451
Chris Lilliendahl <christiank20@ru.is>
2022-11-08 17:07:46 +0000

    Stripping default content

--- sorter-app/src/App.tsx
+++ sorter-app/src/App.tsx
@@ -1,33 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import sunna from './sunna.png'
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={sunna} className="App-logo" alt="logo" />
-        <p>
-          Suna karaadotir - kongur islands.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.facebook.com/sunna.karadottir"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          facebook
-        </a>
-        <a
-          className="App-link"
-          href="https://www.instagram.com/sunnakara/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Insta :*
-        </a>
-      </header>
+    <div className="bg-slate-700 min-h-screen flex flex-col items-center justify-center">
+      
+
+      <p className="text-center text-white ">MjÁ</p>
     </div>
   );
 }
