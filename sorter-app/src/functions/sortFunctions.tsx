5c7d371f666553ab6ea1b92db0aab30cc53de78a
Chris Lilliendahl <christiank20@ru.is>
2022-11-09 14:57:25 +0000

    Swap helpar function

--- sorter-app/src/functions/sortFunctions.tsx
+++ sorter-app/src/functions/sortFunctions.tsx
@@ -1,7 +1,10 @@
 import { bubbleProps, Numbers, shuffleProps } from "../types";
 import { sleep } from "./sleep";
 
-
+// helper function, swap items in given array at givend indecies
+function swap(arr: number[], idx1: number, idx2: number) {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+}
 
 //Fisher-Yates shuffle courtesy of https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 export async function shuffle(props: shuffleProps) {
@@ -18,10 +21,7 @@ export async function shuffle(props: shuffleProps) {
     currentIndex++;
 
     // And swap it with the current element.
-    [arrayCopy[currentIndex], arrayCopy[randomIndex]] = [
-      arrayCopy[randomIndex],
-      arrayCopy[currentIndex],
-    ];
+    swap(arrayCopy, currentIndex, randomIndex);
 
     await sleep(delay);
     setArray((p) => ({
@@ -39,7 +39,10 @@ export async function shuffle(props: shuffleProps) {
   return;
 }
 
-
 export async function bubbleSort(props: bubbleProps) {
-  
+  const { numbers, setArray, delay } = props;
+  let arrayCopy = numbers.array;
+  const length = arrayCopy.length;
+  // let currentIndex = 0;
+  // let randomIndex: number;
 }
